39f5ac3b1976cff25a304932d27328421c539ee6
branfts <support@june07.com>
2024-07-22 23:55:53 -0700

    Updated by A10x Service

---
+++ public/u/meriarreghini.js
@@ -0,0 +1,14 @@
+
+const links = [
+    {
+        "url": "https://www.tiktok.com/@mariaarreghini"
+    },
+    {
+        "url": "https://www.threads.net/@meriarreghini"
+    },
+    {
+        "url": "https://www.instagram.com/meriarreghini/"
+    }
+];
+const rules = [];
+export { links, rules };

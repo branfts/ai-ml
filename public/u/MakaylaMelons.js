57c9657a3418ddc43b99f49254d1f1964fa36e0e
branfts <support@june07.com>
2024-07-27 11:08:39 -0700

    Updated by A10x Service

---
+++ public/u/MakaylaMelons.js
@@ -0,0 +1,32 @@
+
+const links = [
+    {
+        "url": "https://onlyfans.com/no_ppv"
+    },
+    {
+        "url": "https://fansly.com/callme_k_mellons/posts"
+    },
+    {
+        "url": "https://cash.app/$callmekmellons"
+    },
+    {
+        "url": "https://linktr.ee/callmekmellons"
+    },
+    {
+        "url": "https://onlyfans.com/callme_k_melons"
+    },
+    {
+        "url": "https://www.reddit.com/r/makyalamelons/"
+    },
+    {
+        "url": "https://www.tiktok.com/@themakaylamelons?lang=en"
+    },
+    {
+        "url": "https://www.instagram.com/kmellons/"
+    },
+    {
+        "url": "https://twitter.com/callme_k_melons"
+    }
+];
+const rules = [];
+export { links, rules };
